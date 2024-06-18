3e7a25076f12ca82134f0385cfabb9b6e9a9afd4
asifalam515 <asibulalam515@gmail.com>
2024-06-18 17:04:38 +0600

    project setup started

---
+++ mongoose_setup/src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from 'express'
+const app:Application = express()
+const port = 3000
+import cors from 'cors'
+
+
+// parser or middleware
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req:Request, res:Response) => {
+  res.send('Hello World!')
+})
+
+export default app
